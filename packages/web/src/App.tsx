9c4279b0fe774fa85b990f5cf01c22016693fe1d
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2022-07-18 15:25:29 +0200

    Reference shared module in React app

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { add } from "@example-app/shared";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => console.log(add(1, 2))}>Run add function</button>
     </div>
   );
 }
